fd8aeda250bb49d92d29dc12dccfafbf5a74891a
Mason Lapine <masonlapine@pop-os.localdomain>
2020-09-18 12:38:57 -0400

    finished program

--- cost.py
+++ cost.py
@@ -3,6 +3,7 @@ import csv
 import os 
 from datetime import *
 cities = {"rochester": (-99, 27), "buffalo": (-163, 7), "syracuse": (-16, 19), "watertown": (-10, 70), "utica": (32, 23), "glens_falls": (116, 38), "ithica": (-40, -25), "albany": (106, -9), "elmira": (-53, -50), "binghamton": (-4, -49), "kingston": (99,-68), "nyc": (97, -149)}
+population = {"rochester":  205695,"ithica": 30837, "buffalo":  255284, "syracuse": 142327, "watertown": 24838, "utica": 59750, "glens_falls": 14262, "albany": 96460, "elmira": 27054, "binghamton": 44399, "kingston": 22793, "nyc": 8336817}
 phases = {"phase1": date(2020, 6, 8), "phase2": date(2020, 6, 22), "phase3": date(2020,7,6), "phase4": date(2020, 7, 20)}
 def getChords(x, y):
 	#  This is a helper function to show coordinates as a click method.
@@ -38,61 +39,15 @@ def gasPricesCalc(finList):
 	for j in finList:
 		#print(j)
 		if last != j[0]:
-			theDate = j[0][1:11]
-			#print(str(theDate))
-			year, month, day = theDate.split('-')
-			year = int(year)
-			month = int(month)
-			day = int(day)
-			if (date(year, month, day) < phases["phase1"]):
-				turtle.color("orange")
-				turtle.up()
-				turtle.goto(-200, 80)
-				turtle.down()
-				turtle.write("PRE COVID-19 Phases")
-				turtle.up()
-			if (date(year, month, day) >= phases["phase1"]) and (date(year, month, day) < phases['phase2']):
-				turtle.color("orange")
-				turtle.up()
-				turtle.goto(-200, 80)
-				turtle.down()
-				turtle.write("COVID-19 Phase 1")
-				turtle.up()
-			if (date(year, month, day) >= phases["phase2"]) and (date(year, month, day) < phases['phase3']):
-				turtle.color("orange")
-				turtle.up()
-				turtle.goto(-200, 80)
-				turtle.down()
-				turtle.write("COVID-19 Phase 2")
-				turtle.up()
-			if (date(year, month, day) >= phases["phase3"]) and (date(year, month, day) <= phases['phase4']):
-				turtle.color("orange")
-				turtle.up()
-				turtle.goto(-200, 80)
-				turtle.down()
-				turtle.write("COVID-19 Phase 3")
-				turtle.up()
-			if (date(year, month, day) > phases["phase4"]):
-				turtle.color("orange")
-				turtle.up()
-				turtle.goto(-200, 80)
-				turtle.down()
-				turtle.write("COVID-19 Phase 4")
-				turtle.up()
 			turtle.goto(-270, 170)
 			turtle.color("black")
 			turtle.down()
 			turtle.write("Oil Barrel Price")
 			turtle.up()
-			turtle.goto(200, 170)
-			turtle.color("red")
-			turtle.down()
-			turtle.write("Red = Gas Price")
-			turtle.up()
 			# This os.system command WILL NOT work on other computers.
 			# The line below is meant to generate png images based on what the program highlights gas prices.
 			# It requires extra dependencies that are needed via linux 
-			# os.system('sleep 1s && import -window "$(xdotool getactivewindow)" ~/Documents/gasData/frames/' + j[0] + ".png")
+			os.system('sleep 1s && import -window "$(xdotool getactivewindow)" ~/Documents/gasData/frames/' + j[0] + ".png")
 			last = j[0]
 			lastOil =j[2]
 			turtle.reset()
@@ -137,26 +92,43 @@ def gasPricesCalc(finList):
 				turtle.write(str(lastOil))
 				turtle.up()
 			continue
-		radius = -(3 - (float(j[1]))*10)
-		radius = abs((21 - radius)*5)
-		radius = (21 - radius)*1.5
+		# Circle Sizes Math
+		priceMax = 2.79
+		priceMin = 1.94
+		thisRange = priceMax - priceMin
+		thisRange = thisRange / 100
+		scale = thisRange
+		
+		cityPrice = float(j[1])
+		cityPriceRad = cityPrice - priceMin
+		cityPriceRad = cityPriceRad *(1/scale)
+		radius = cityPriceRad / 3.5
+		
+		# End Math
+		# Begin Color Math
+		maxPop = 9000000
+		minPop = 13000
+		maxColor = 255
+		minColor = 0
+		scaleColor = 0.000888502
+		turtle.colormode(255)
+		
 		if j[3] in cities:
 			print(j[0][1:11] + "," + j[1] + "," + j[2] + "," + j[3])
 			toCity(j[3])
-			turtle.color("red")
+			if j[3] == "nyc":
+				turtle.color("black")
+				turtle.pensize(3)
+			else:
+				turtle.pensize(3)
+				# Darker color = More people that live in city
+				turtle.color(255 - abs(int(population[j[3]]*scaleColor)), 0,0)
 			turtle.showturtle()
 			turtle.right(90)
 			turtle.forward(radius)
-			#print(str(j[1]) + " -- " +str(radius))
 			turtle.right(270)
 			turtle.down()
 			turtle.circle(radius)
-			turtle.up()
-			turtle.forward(-35)
-			turtle.down()
-			turtle.write(str(j[1]))
-			turtle.up()
-			turtle.backward(-35)
 			turtle.up()
 			turtle.home()
 			turtle.hideturtle()
